87d740ee581fc6ee7080abdca4a8b16017b0cb19
Trần Hiền <thuhienchymte932k4@gmail.com>
2025-03-10 23:46:46 +0700

    Add files via upload

---
+++ HinhHoc/Main.java
@@ -0,0 +1,14 @@
+package hinhhoc;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		Shapes[] shapes = new Shapes[3];
+//		shapes[0] = new Circle();
+//		shapes[1] = new Triangle();
+//		shapes[2] = new Rectangle();
+//		for(int i = 0; i < shapes.length; i++)
+//			System.out.println("Dien tich la: %5.3f\n");
+	}
+	
+}
